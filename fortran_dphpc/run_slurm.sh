53604e314d79eed99ef84af4341a01e0bdbf9090
Lorenzo Celli <lcelli@student-cluster2.inf.ethz.ch>
2025-12-06 18:40:59 +0100

    Add slurm script

---
+++ fortran_dphpc/run_slurm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=dphpc
+#SBATCH --gpus=5060ti:1
+#SBATCH --output=logs/%x-%j.out
+#SBATCH --error=logs/%x-%j.err
+
+source /etc/profile
+
+module add cuda/12.8
+
+nvidia-smi
+
+# Activate virtual environment
+source .venv/bin/activate
+
+# Clear DaCe cache to avoid stale data
+rm -rf .dacecache/simple_masked_reduction_1
+
+# Clear logs
+rm -rf logs/*
+
+export CUDACXX=/cluster/data/cuda/12.8.1/bin/nvcc
+python -m fortran_dphpc.reduce
