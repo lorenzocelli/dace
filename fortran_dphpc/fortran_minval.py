e4e0015aa46f2aea6bc1c827079e794fc239aee7
Lorenzo Celli <lorenzo.celli00@gmail.com>
2025-12-29 16:45:59 +0100

    Update scripts

--- fortran_dphpc/fortran_minval.py
+++ fortran_dphpc/fortran_minval.py
@@ -1,11 +1,9 @@
 """
 Run with:
-python -m fortran_dphpc.maxval
+python -m fortran_dphpc.fortran_minval
 """
 from dace.frontend.fortran.fortran_parser import create_singular_sdfg_from_string
 from dace.transformation.interstate import GPUTransformSDFG
-from dace.transformation.dataflow import GPUTransformMap
-
 import numpy as np
 
 SQUARE_FORTRAN_SOURCE = """SUBROUTINE minval_full(d, res)
@@ -20,4 +18,13 @@ END SUBROUTINE minval_full
 sdfg = create_singular_sdfg_from_string(SQUARE_FORTRAN_SOURCE, 'minval_full', True)
 sdfg.apply_transformations(GPUTransformSDFG)
 sdfg.simplify()
+
+# Instrument the SDFG
+# import dace
+# for state in sdfg.nodes():
+#     state.instrument = dace.InstrumentationType.GPU_Events
+#     for node in state.nodes():
+#         if isinstance(node, dace.sdfg.nodes.MapEntry):
+#             node.instrument = dace.InstrumentationType.GPU_Events
+
 sdfg.compile()
--- fortran_dphpc/py_minval.py
+++ fortran_dphpc/py_minval.py
@@ -1,26 +1,23 @@
 """
 Reduction examples using the python frontend of DaCe.
 
-python -m fortran_dphpc.reduce
+python -m fortran_dphpc.py_minval
 """
 
-import os
 import numpy as np
 
 import dace
-
-from dace.transformation.dataflow import GPUTransformMap
 from dace.transformation.interstate import GPUTransformSDFG
 
 N = dace.symbol("N")
 
 @dace.program
-def simple_reduction(A: dace.float64[N], R: dace.float64[1]):
+def minval_py(A: dace.float64[N], R: dace.float64[1]):
     R[0] = dace.reduce(lambda a, b: min(a, b), A, identity=np.inf)
 
 
 print("Creating SDFG...")
-sdfg = simple_reduction.to_sdfg()
-sdfg.apply_transformations(GPUTransformMap)
+sdfg = minval_py.to_sdfg()
+sdfg.apply_transformations(GPUTransformSDFG)
 sdfg.simplify()
 sdfg.compile()
